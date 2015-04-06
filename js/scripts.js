9b19e7ad48bef0febe521c25012005c201d7909c
Abdelnoor & Baumgart <student@epicodus.com>
2015-04-06 11:43:30 -0700

    Add webpage for queen attack

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,24 @@ var queenAttack = function(x1, y1, x2 ,y2) {
         return false;
     }
 };
+
+$(document).ready(function() {
+    $("form#queen-attack").submit(function(event) {
+        var x1 = ($("input#x1").val());
+        var y1 = ($("input#y1").val());
+        var x2 = ($("input#x2").val());
+        var y2 = ($("input#y2").val());
+        var result = queenAttack(x1, y1, x2, y2);
+
+        $(".result").text("");
+        if (result) {
+            $(".result1").text("The queen can attack the selected position");
+        } else {
+            $(".result1").text("The queen cannot attack the selected position");
+        }
+
+        $("#result").show();
+        event.preventDefault();
+
+    });
+});
